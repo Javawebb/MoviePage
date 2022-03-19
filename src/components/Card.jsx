6e3cd714cfa021b270dd2b56a8d0d16fe6342260
Max <Maksartykov55@gmail.com>
2022-03-19 20:31:47 +0500

    second-commit

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,21 +1,20 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
-export const Card = () => {
+export const Card = ({item}) => {
   return (
-    <div>
-        <div className="card w-74 bg-base-100 shadow-xl">
-  <figure><img src="https://api.lorem.space/image/shoes?w=400&h=225" alt="Shoes" /></figure>
+    <div className="card w-74 bg-base-100 shadow-xl">
+  <figure><img src="https://api.lorem.space/image/movie?w=400&h=225" alt="Shoes" /></figure>
   <div className="card-body">
     <h2 className="card-title">
-      Shoes!
-      <div className="badge badge-error">8.9 &#10029;</div>
+      {item.title}
+      <div className="badge badge-error">{item.vote_average}&#9733;</div>
     </h2>
-    <p>If a dog chews shoes whose shoes does he choose?</p>
-    <div className="card-actions justify-start">
-      <div className="badge badge-outline">Watch</div>
+    <p>{item.overview.substring(0,100)} . . . . . .</p>
+    <div className="card-actions justify-end">
+      <Link to={`/movie/${item.id}`} className='btn btn-sm btn-primary mt-4'>Watch</Link>
     </div>
   </div>
 </div>
-    </div>
   )
 }
--- src/pages/Movie.jsx
+++ src/pages/Movie.jsx
@@ -1,6 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useParams } from 'react-router-dom'
 
-export const Movie = () => {
+export const Movie = ({movies}) => {
+
+  const {id} = useParams()
+  let movie = movies.filter(item => item.id === +id)[0]
+
+  if(!movie) return <button className='btn loading'></button>
+
+  console.log(id,movie)
   return (
     <div>
         <div className="hero min-h-screen bg-base-200">
@@ -8,20 +16,20 @@ export const Movie = () => {
     <img src="https://api.lorem.space/image/movie?w=260&h=400" className="max-w-sm rounded-lg shadow-2xl" />
     <div>
         <div className="">
-            <h1 className="text-5xl font-bold">Box Office News!</h1>
-            <div class="badge badge-error gap-2 mt-2">8.9 &#10029;</div>
+            <h1 className="text-5xl font-bold">{movie.title}</h1>
+            <div class="badge badge-error gap-2 mt-2">{movie.vote_average} &#10029;</div>
             </div>
             <div className="divider"></div>
         <ul className='mt-5 flex flex-col gap-2'>
             <h1 className='text-2xl font-bold mb-2 text-succes'>About Movie</h1>
-            <li>Original-Title: <strong>Spider-Man Far from Home</strong></li>
-            <li>Popularity: <strong>50,000,000</strong></li>
-            <li>Votes: <strong>20,000</strong></li>
-            <li>Realise-date: <strong>2019-06-26</strong></li>
-            <li>Language: <strong>ENG</strong></li>
+            <li>Original-Title: <strong>{movie.original_title}</strong></li>
+            <li>Popularity: <strong>{movie.popularity}</strong></li>
+            <li>Votes: <strong>{movie.vote_count}</strong></li>
+            <li>Realise-date: <strong>{movie.release_date}</strong></li>
+            <li>Language: <strong>{movie.original_language}</strong></li>
         </ul>
         <div className="divider mt-5"></div>
-        <p className="text-lg">Spider-Man: Far From Home is a 2019 American superhero film based on the Marvel Comics character Spider-Man, co-produced by Columbia Pictures and Marvel Studios, and distributed by Sony Pictures Releasing. It is the sequel to Spider-Man: Homecoming (2017) and the 23rd film in the Marvel Cinematic Universe (MCU). The film was directed by Jon Watts, written by Chris McKenna and Erik Sommers, and stars Tom Holland as Peter Parker / Spider-Man, alongside Samuel L. Jackson, Zendaya, Cobie Smulders, Jon Favreau, J. B. Smoove, Jacob Batalon, Martin Starr, Marisa Tomei, and Jake Gyllenhaal.</p>
+        <p className="text-lg">{movie.overview}</p>
     </div>
   </div>
 </div>
